ab633bc307fd78d38e6e821756a5d372d85e1038
Julia <2509juliasanchez@gmail.com>
2023-01-12 11:33:03 -0300

    feat: add files

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputWeight = e.target.querySelector('#weight');
+    const inputHeight = e.target.querySelector('#height');
+
+    const weight = Number(inputWeight.value);
+    const height = Number(inputHeight.value);
+
+    if (!weight) {
+        setResult('Peso inválido', false);
+        return;
+    }
+
+    if (!height) {
+        setResult('Altura inválida', false);
+        return;
+    }
+
+    const bmi = getBMI(weight, height);
+
+    console.log(bmi);
+});
+
+function getBMI(weight, height) {
+    const bmi = weight / height ** 2;
+    return bmi.toFixed(2);
+}
+
+function createP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResult(msg, isValid) {
+    const result = document.querySelector("#result");
+    result.innerHTML = '';
+
+    const p = createP();
+    p.innerHTML = msg;
+    result.appendChild(p);
+}
